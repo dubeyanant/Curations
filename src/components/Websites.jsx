b61fe73026c03fa4bbef8c77e17483d7b42903ab
dubeyanant <anantkumar312@gmail.com>
2023-11-23 00:41:43 +0530

    feat(Website): :sparkles: add card tile

--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -1,4 +1,34 @@
+import Card from "./tiles/Card";
+
 const Websites = () => {
-  return <div>Websites</div>;
+  return (
+    <>
+      <p className="text-lg">
+        These are some coool websites that I like. This presents several fun and
+        useful sites which you might want to check out.
+      </p>
+      <div className="mt-12 grid grid-cols-3">
+        <Card
+          url="https://learnuiux.in/"
+          name="Learn UI/UX"
+          urlImg="https://images.unsplash.com/photo-1690228254548-31ef53e40cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          category="Website"
+        />
+        <Card
+          url="https://lucide.dev/icons/"
+          name="Lucide Icons"
+          urlImg="https://images.unsplash.com/photo-1506729623306-b5a934d88b53?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          category="Website"
+        />
+        <Card
+          url="https://tailwindcss.com/"
+          name="TailwindCSS"
+          urlImg="https://images.unsplash.com/photo-1529661197280-63dc545366c8?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          category="Website"
+        />
+      </div>
+    </>
+  );
 };
+
 export default Websites;
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ export default {
         success: "#32A853",
       },
       grays: {
+        transparent: "#00000000",
         white: "#FFFFFF",
         black: "#000000",
         gray: "#7A7A7A",
