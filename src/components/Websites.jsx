cfa8b13ec8f89d4fd32b244e92ff6516c0406993
dubeyanant <anantkumar312@gmail.com>
2023-11-23 22:30:32 +0530

    feat(Website): :sparkles: map website.jsx to websites.json

--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -1,31 +1,22 @@
 import Card from "./tiles/Card";
+import websitesJson from "../assets/websites.json";
 
 const Websites = () => {
   return (
     <>
       <p className="text-lg">
-        These are some coool websites that I like. This presents several fun and
+        These are some cool websites that I like. This presents several fun and
         useful sites which you might want to check out.
       </p>
-      <div className="mt-12 grid grid-cols-3">
-        <Card
-          url="https://learnuiux.in/"
-          name="Learn UI/UX"
-          urlImg="https://images.unsplash.com/photo-1690228254548-31ef53e40cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          category="Website"
-        />
-        <Card
-          url="https://lucide.dev/icons/"
-          name="Lucide Icons"
-          urlImg="https://images.unsplash.com/photo-1506729623306-b5a934d88b53?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          category="Website"
-        />
-        <Card
-          url="https://tailwindcss.com/"
-          name="TailwindCSS"
-          urlImg="https://images.unsplash.com/photo-1529661197280-63dc545366c8?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          category="Website"
-        />
+      <div className="mt-12 grid grid-cols-3 gap-5">
+        {websitesJson.map((website, index) => (
+          <Card
+            key={index}
+            url={website.url}
+            name={website.name}
+            urlImg={website.urlImg}
+          />
+        ))}
       </div>
     </>
   );
