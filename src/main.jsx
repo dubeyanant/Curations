f4d74910e6089a576434e73a19670c84338eb496
dubeyanant <anantkumar312@gmail.com>
2023-11-25 03:07:19 +0530

    feat: :heavy_plus_sign: add chakra UI dependency

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./config/store.js";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+
+delete theme.styles.global;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <ChakraProvider theme={theme} resetCSS={false}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
