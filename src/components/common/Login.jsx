8d950dd22e32120ed2f9011122945428ffacb882
dubeyanant <anantkumar312@gmail.com>
2023-11-27 14:29:30 +0530

    feat: :sparkles: add login form validation

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -9,13 +9,19 @@ import {
   Button,
   FormControl,
   FormLabel,
+  FormHelperText,
   Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Login = ({ isOpen, onClose, onLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
 
   // Reset state when the modal is opened
   useEffect(() => {
@@ -30,6 +36,9 @@ const Login = ({ isOpen, onClose, onLogin }) => {
     onClose();
   };
 
+  const isUsernameValid = /^[a-z]{3,}$/.test(username);
+  const isPasswordValid = password.trim().length >= 3;
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -37,26 +46,42 @@ const Login = ({ isOpen, onClose, onLogin }) => {
         <ModalHeader>Enter Login Details</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={!isUsernameValid}>
             <FormLabel>Username</FormLabel>
             <Input
-              placeholder="your username"
+              placeholder="Enter username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
+            <FormHelperText>minimum three smallcase characters</FormHelperText>
           </FormControl>
-          <FormControl mt={4} isRequired>
+          <FormControl mt={4} isRequired isInvalid={!isPasswordValid}>
             <FormLabel>Password</FormLabel>
-            <Input
-              placeholder="your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+
+            <InputGroup size="md">
+              <Input
+                type={show ? "text" : "password"}
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                  {show ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormHelperText>minimum three characters</FormHelperText>
           </FormControl>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="orange" mr={3} onClick={handleLogin}>
+          <Button
+            colorScheme="orange"
+            mr={3}
+            onClick={handleLogin}
+            isDisabled={!isUsernameValid || !isPasswordValid}
+          >
             Login
           </Button>
           <Button onClick={onClose}>Cancel</Button>
