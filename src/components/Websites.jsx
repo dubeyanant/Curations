29d6864b0bfd7025b249d232f98674b90a2cbab9
dubeyanant <anantkumar312@gmail.com>
2023-11-28 20:46:12 +0530

    fix(Website): :bug: website card spacing

--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -23,7 +23,7 @@ const Websites = () => {
             These are some cool websites that I like. This presents several fun
             and useful sites which you might want to check out.
           </p>
-          <div className="mt-12 grid grid-cols-3 gap-5">
+          <div className="mt-12 flex justify-between flex-wrap gap-x-1 gap-y-4">
             {isEditIcon && <AddCard />}
             {filteredData.map((website) => (
               <Card
