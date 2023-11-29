e27c895832c1ae0c1cf3ac389cd230630c5989df
dubeyanant <anantkumar312@gmail.com>
2023-11-29 17:27:15 +0530

    style(Sidebar): :poop: made sidebar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import SideBar from "./components/SideBar";
 import Home from "./components/Home";
@@ -7,21 +8,31 @@ import { Center, Divider } from "@chakra-ui/react";
 import { AlignJustify } from "lucide-react";
 
 function App() {
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
   return (
     <div className="px-[60px] xl:px-[120px] 2xl:px-[160px] py-10 md:py-20 lg:py-28 xl:py-32 flex flex-col md:flex-row gap-2 md:gap-16 xl:gap-20 items-start md:items-stretch">
       <div className="flex items-center justify-between w-full visible md:hidden">
         <h2 className="text-2xl font-bold">Curations✨</h2>
-        <button>
+        <button onClick={toggleSidebar}>
           <AlignJustify />
         </button>
       </div>
-      <div className="fixed md:flex flex-col hidden md:visible">
+      <div
+        className={`md:fixed md:flex flex-col ${
+          isSidebarVisible ? "visible" : "hidden"
+        }`}
+      >
         <SideBar />
       </div>
-      <Center className=" ml-56">
+      <Center className="ml-56">
         <Divider orientation="vertical" />
       </Center>
-      <div>
+      <div className={`${isSidebarVisible ? "hidden" : "visible"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/websites" element={<Websites />} />
